99472a089117be0403cc61b913e087559c3d12d6
lichanglin <1430136178@qq.com>
2025-06-25 13:59:14 +0800

    示例代码格式化

--- example/main.cpp
+++ example/main.cpp
@@ -9,75 +9,75 @@
 class LogService : public QObject
 {
 	Q_OBJECT
-        public:
-			LogService(QObject* parent = nullptr) : QObject(parent)
-            {
-                responseObject = new ResponseObject(this, "logService", QMessageBus::instance());
-                connect(responseObject, &ResponseObject::receiveRequest, this, &LogService::onReceiveRequest);
-            }
-            ~LogService()
-            {
-                qDebug() << "LogService destroyed";
-			}
+public:
+	LogService(QObject* parent = nullptr) : QObject(parent)
+	{
+		responseObject = new ResponseObject(this, "logService", QMessageBus::instance());
+		connect(responseObject, &ResponseObject::receiveRequest, this, &LogService::onReceiveRequest);
+	}
+	~LogService()
+	{
+		qDebug() << "LogService destroyed";
+	}
 private slots:
-    void onReceiveRequest(const RequestMessage& requestMessage)
-            {
-                qDebug() << "LogService received message:" << requestMessage.requestName << requestMessage.command << requestMessage.messageId << requestMessage.message;
-                if (requestMessage.command == "debug")
-                {
-                    responseObject->reply(requestMessage.requestName, requestMessage.messageId, QString("log success"));
-                }
-            }
+	void onReceiveRequest(const RequestMessage& requestMessage)
+	{
+		qDebug() << "LogService received message:" << requestMessage.requestName << requestMessage.command << requestMessage.messageId << requestMessage.message;
+		if (requestMessage.command == "debug")
+		{
+			responseObject->reply(requestMessage.requestName, requestMessage.messageId, QString("log success"));
+		}
+	}
 
 private:
-    //std::shared_ptr<ResponseObject> responseObject;
-    ResponseObject* responseObject;
+	ResponseObject* responseObject;
 };
 
 //c++20 协程风格的同步请求函数
 QCoro::Task<void> syncRequest(RequestObject* request, const QString& serviceName, const QString& command, const QVariant& message)
 {
-    int messageId = request->sendRequest(serviceName, command, message);
-    auto listener = qCoroSignalListener(request, &RequestObject::receiveReply, std::chrono::seconds(1));
-    QCORO_FOREACH(auto messages, listener)
-    {
+	int messageId = request->sendRequest(serviceName, command, message);
+	auto listener = qCoroSignalListener(request, &RequestObject::receiveReply, std::chrono::seconds(1));
+	QCORO_FOREACH(auto messages, listener)
+	{
 		auto& [responseMessageId, responseMessage] = messages;
-        if (responseMessageId == messageId)
-        {
+		if (responseMessageId == messageId)
+		{
 			qDebug() << "syncRequest Received response for messageId" << responseMessageId << ":" << responseMessage;
-            co_return;
-        }
-    }
-    qDebug() << "Received response Timeout";
-    co_return;
+			co_return;
+		}
+	}
+	qDebug() << "Received response Timeout";
+	co_return;
 }
 int main(int argc, char* argv[])
 {
-    QCoreApplication app(argc, argv);
+	QCoreApplication app(argc, argv);
 
-    QThread thread;
-    thread.start();
+	QThread thread;
+	thread.start();
 
 	LogService* logService = new LogService();
 	logService->moveToThread(&thread);
 
-    RequestObject request;
+	RequestObject request;
 
 	//不需要响应对象的请求
-    request.sendRequest("logService", "info", QString("Hello, World!"));
+	request.sendRequest("logService", "info", QString("Hello, World!"));
 
 	//需要响应对象的请求
-    syncRequest(&request, "logService", "debug", QString("Hello, World!"));
+	syncRequest(&request, "logService", "debug", QString("Hello, World!"));
 
-    syncRequest(&request, "logService", "info", QString("Hello, World!"));
+	syncRequest(&request, "logService", "info", QString("Hello, World!"));
 
-    QTimer::singleShot(3000, &app, [&]() {
+	QTimer::singleShot(3000, &app, [&] ()
+	{
 		logService->deleteLater();
-        thread.quit();
-        thread.wait();
-        app.quit();
+		thread.quit();
+		thread.wait();
+		app.quit();
 	});
-    return app.exec();
+	return app.exec();
 }
 
 #include "main.moc"
--- src/QMessageBus.cpp
+++ src/QMessageBus.cpp
@@ -6,100 +6,100 @@
 
 QMessageBus* QMessageBus::instance()
 {
-    static QMessageBus instance;
-    return &instance;
+	static QMessageBus instance;
+	return &instance;
 }
 
 QMessageObject::QMessageObject(QObject* parent, const QString& name, QMessageBus* eventBus)
-    : QObject(parent)
+	: QObject(parent)
 {
-    m_name = name.isEmpty() ? QUuid::createUuid().toString(QUuid::Id128) : name;
-    m_eventBus = eventBus != nullptr ? eventBus : QMessageBus::instance();
-    m_eventBus->registerEventObject(this);
+	m_name = name.isEmpty() ? QUuid::createUuid().toString(QUuid::Id128) : name;
+	m_eventBus = eventBus != nullptr ? eventBus : QMessageBus::instance();
+	m_eventBus->registerEventObject(this);
 }
 
 QMessageObject::~QMessageObject()
 {
-    m_eventBus->unregisterEventObject(this);
+	m_eventBus->unregisterEventObject(this);
 }
 
 void QMessageObject::send(const QString& targetName, const QVariantHash& message)
 {
-    m_eventBus->routeMessage(targetName, message);
+	m_eventBus->routeMessage(targetName, message);
 }
 
 void QMessageBus::registerEventObject(QMessageObject* eventObject)
 {
-    QWriteLocker locker(&m_rwlock);
-    m_eventObjects.insert(eventObject->name(), eventObject);
+	QWriteLocker locker(&m_rwlock);
+	m_eventObjects.insert(eventObject->name(), eventObject);
 }
 
 void QMessageBus::unregisterEventObject(QMessageObject* eventObject)
 {
-    QWriteLocker locker(&m_rwlock);
-    m_eventObjects.remove(eventObject->name());
+	QWriteLocker locker(&m_rwlock);
+	m_eventObjects.remove(eventObject->name());
 }
 
 void QMessageBus::routeMessage(const QString& targetName, const QVariantHash& message)
 {
-    QReadLocker locker(&m_rwlock);
-    auto iter = m_eventObjects.find(targetName);
-    if (iter != m_eventObjects.cend())
-    {
-        Q_EMIT iter.value()->receive(message);
-    }
+	QReadLocker locker(&m_rwlock);
+	auto iter = m_eventObjects.find(targetName);
+	if (iter != m_eventObjects.cend())
+	{
+		Q_EMIT iter.value()->receive(message);
+	}
 }
 
 RequestObject::RequestObject(QObject* parent, const QString& name, QMessageBus* QMessageBus)
-    : QMessageObject(parent, name, QMessageBus)
+	: QMessageObject(parent, name, QMessageBus)
 {
-    connect(this, &QMessageObject::receive, this, &RequestObject::onReceive, Qt::QueuedConnection);
+	connect(this, &QMessageObject::receive, this, &RequestObject::onReceive, Qt::QueuedConnection);
 }
 
 int RequestObject::sendRequest(const QString& targetName, const QString& command, const QVariant& message)
 {
-    QVariantHash data;
-    data.insert("request", name());
-    data.insert("command", command);
-    data.insert("message", message);
-    int messageId = generateMessageId();
-    data.insert("messageId", messageId);
-    send(targetName, data);
-    return messageId;
+	QVariantHash data;
+	data.insert("request", name());
+	data.insert("command", command);
+	data.insert("message", message);
+	int messageId = generateMessageId();
+	data.insert("messageId", messageId);
+	send(targetName, data);
+	return messageId;
 }
 
 void RequestObject::onReceive(const QVariantHash& message)
 {
-    int messageId = message.value("messageId").toInt();
-    QVariant data = message.value("message");
-    emit receiveReply(messageId, data);
+	int messageId = message.value("messageId").toInt();
+	QVariant data = message.value("message");
+	emit receiveReply(messageId, data);
 }
 
 int RequestObject::generateMessageId()
 {
-    return m_messageId++;
+	return m_messageId++;
 }
 
 ResponseObject::ResponseObject(QObject* parent, const QString& name, QMessageBus* QMessageBus)
-    : QMessageObject(parent, name, QMessageBus)
+	: QMessageObject(parent, name, QMessageBus)
 {
-    connect(this, &QMessageObject::receive, this, &ResponseObject::onReceive, Qt::QueuedConnection);
+	connect(this, &QMessageObject::receive, this, &ResponseObject::onReceive, Qt::QueuedConnection);
 }
 
 void ResponseObject::reply(const QString& requestName, int messageId, const QVariant& message)
 {
-    QVariantHash data;
-    data.insert("messageId", messageId);
-    data.insert("message", message);
-    send(requestName, data);
+	QVariantHash data;
+	data.insert("messageId", messageId);
+	data.insert("message", message);
+	send(requestName, data);
 }
 
 void ResponseObject::onReceive(const QVariantHash& message)
 {
-    QString requestName = message.value("request").toString();
-    QString command = message.value("command").toString();
-    int messageId = message.value("messageId").toInt();
-    QVariant data = message.value("message");
-    RequestMessage requestMessage{ requestName, command ,messageId ,data };
-    emit receiveRequest(requestMessage);
+	QString requestName = message.value("request").toString();
+	QString command = message.value("command").toString();
+	int messageId = message.value("messageId").toInt();
+	QVariant data = message.value("message");
+	RequestMessage requestMessage{ requestName, command ,messageId ,data };
+	emit receiveRequest(requestMessage);
 }
